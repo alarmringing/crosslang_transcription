5949ac3b4a8aec649ffb17975cbc2fdc934543e1
Jihee Hwang <jiheeh@stanford.edu>
2017-04-28 01:39:52 -0700

    You can specify output directories now.

--- prototype_transcribe.py
+++ prototype_transcribe.py
@@ -1,19 +1,28 @@
 import argparse
 import io
+import os
 import sys
 import soundfile as sf
 
 PAYLOAD_LIMIT = 10485760
 OVERLAP = PAYLOAD_LIMIT/float(20)
 
-def transcribe_file(speech_file):
+def transcribe_file(speech_file, output_dir):
 	"""Transcribe the given audio file."""
 	from google.cloud import speech
 	speech_client = speech.Client()
 
-	print("speech_file looks like ", speech_file)
-	save_file = open("Output.txt", "w")
+	"""create and open text file to save transcription"""
+	if not os.path.exists(os.path.dirname(output_dir)):
+	    try:
+	        os.makedirs(os.path.dirname(output_dir))
+	    except OSError as exc: # Guard against race condition
+	        if exc.errno != errno.EEXIST:
+	            raise
+	save_file = open(output_dir + os.path.basename(speech_file).split('.')[0] + '.txt', "w")
 	text = ''
+
+	"""Slice audio into # of blocks, then send to google cloud for analysis"""
 	for audio in sf.blocks(speech_file, blocksize=PAYLOAD_LIMIT, overlap=OVERLAP):
 		sf.SoundFile('buffer.wav', 'w', 44100, 1, 'PCM_16').write(audio.sum(axis=1) / float(2))
 		content = io.open('buffer.wav', 'rb').read()
@@ -24,19 +33,12 @@ def transcribe_file(speech_file):
 			sample_rate_hertz=44100)
 		alternatives = audio_sample.recognize('en-US')
 	for alternative in alternatives:
-		#text_file.write("%s" % alternative.transcript)
-		text += '\n' + alternative.transcript
-		#print('Transcript: {}'.format(alternative.transcript))
+		text += alternative.transcript
 
+	"""final save"""
 	save_file.write(text)
 	save_file.close()
 
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser(
-		description=__doc__,
-		formatter_class=argparse.RawDescriptionHelpFormatter)
-	parser.add_argument(
-		'path', help='type audio file path to be recognized')
-	args = parser.parse_args()
-	print("args is ", args)
-	transcribe_file(args.path)
+
+	transcribe_file(sys.argv[1], sys.argv[2])
