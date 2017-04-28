c48b4849935bd10ab8517d7bee49811376ef35ef
Jihee Hwang <jiheeh@stanford.edu>
2017-04-28 01:59:58 -0700

    Added language flag capability, but need to implement real parsers instead of simply reading strings

--- prototype_transcribe.py
+++ prototype_transcribe.py
@@ -7,10 +7,16 @@ import soundfile as sf
 PAYLOAD_LIMIT = 10485760
 OVERLAP = PAYLOAD_LIMIT/float(20)
 
-def transcribe_file(speech_file, output_dir):
+def transcribe_file(lang, speech_file, output_dir):
 	"""Transcribe the given audio file."""
 	from google.cloud import speech
 	speech_client = speech.Client()
+	#set language
+	language = ''
+	if lang == '-e':
+		language = 'en-US'
+	else:
+		language = 'ja-JP'
 
 	"""create and open text file to save transcription"""
 	if not os.path.exists(os.path.dirname(output_dir)):
@@ -23,6 +29,7 @@ def transcribe_file(speech_file, output_dir):
 	text = ''
 
 	"""Slice audio into # of blocks, then send to google cloud for analysis"""
+
 	for audio in sf.blocks(speech_file, blocksize=PAYLOAD_LIMIT, overlap=OVERLAP):
 		sf.SoundFile('buffer.wav', 'w', 44100, 1, 'PCM_16').write(audio.sum(axis=1) / float(2))
 		content = io.open('buffer.wav', 'rb').read()
@@ -31,14 +38,14 @@ def transcribe_file(speech_file, output_dir):
 			source_uri=None,
 			encoding='LINEAR16',
 			sample_rate_hertz=44100)
-		alternatives = audio_sample.recognize('en-US')
+		alternatives = audio_sample.recognize(language)
 	for alternative in alternatives:
 		text += alternative.transcript
 
 	"""final save"""
-	save_file.write(text)
+	save_file.write(text.encode('utf-8'))
 	save_file.close()
 
 if __name__ == '__main__':
 
-	transcribe_file(sys.argv[1], sys.argv[2])
+	transcribe_file(sys.argv[1], sys.argv[2], sys.argv[3])
