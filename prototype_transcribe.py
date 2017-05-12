fa4c363642b74fbe9bd71d17e577eff1f53e61cb
Jihee Hwang <jiheeh@stanford.edu>
2017-05-12 02:27:18 -0700

    Changed some parameter names due to upgrade of google speech python package

--- prototype_transcribe.py
+++ prototype_transcribe.py
@@ -4,48 +4,66 @@ import os
 import sys
 import soundfile as sf
 
-PAYLOAD_LIMIT = 10485760
-OVERLAP = PAYLOAD_LIMIT/float(20)
+#PAYLOAD_LIMIT = 10485760
+PAYLOAD_LIMIT = 2500000
+OVERLAP = PAYLOAD_LIMIT/60
 
-def transcribe_file(lang, speech_file, output_dir):
+def transcribe_file(speech_filepath, output_filepath, keyword_file):
 	"""Transcribe the given audio file."""
 	from google.cloud import speech
 	speech_client = speech.Client()
+
+	"""Read keyword_file, if any."""
+	keywords = [] #empty list
+	with open(keyword_file, 'r') as f:
+		keywords_raw = f.readlines()
+		for i in range(len(keywords_raw)):
+			keywords.append(keywords_raw[i].lower().strip())
+
+	#MULTILINGUAL SUPPORT DISABLED FOR NOW
+	language = 'en-US'
+	'''
 	#set language
 	language = ''
 	if lang == '-e':
 		language = 'en-US'
 	else:
 		language = 'ja-JP'
+	'''
+
 
 	"""create and open text file to save transcription"""
-	if not os.path.exists(os.path.dirname(output_dir)):
-	    try:
-	        os.makedirs(os.path.dirname(output_dir))
-	    except OSError as exc: # Guard against race condition
-	        if exc.errno != errno.EEXIST:
-	            raise
-	save_file = open(output_dir + os.path.basename(speech_file).split('.')[0] + '_output.txt', "w")
+	save_file = open(output_filepath, "w")
 	text = ''
 
 	"""Slice audio into # of blocks, then send to google cloud for analysis"""
 
-	for audio in sf.blocks(speech_file, blocksize=PAYLOAD_LIMIT, overlap=OVERLAP):
+	for audio in sf.blocks(speech_filepath, blocksize=PAYLOAD_LIMIT, overlap=OVERLAP):
+		print("audo.shape is ", audio.shape)
 		sf.SoundFile('buffer.wav', 'w', 44100, 1, 'PCM_16').write(audio.sum(axis=1) / float(2))
 		content = io.open('buffer.wav', 'rb').read()
 		audio_sample = speech_client.sample(
 			content=content,
 			source_uri=None,
 			encoding='LINEAR16',
-			sample_rate_hertz=44100)
-		alternatives = audio_sample.recognize(language)
-	for alternative in alternatives:
-		text += alternative.transcript
+			sample_rate=44100)
+		alternatives = audio_sample.sync_recognize(language_code=language, speech_context=keywords)
+		for alternative in alternatives:
+			text += alternative.transcript
+	
 
 	"""final save"""
 	save_file.write(text.encode('utf-8'))
 	save_file.close()
 
+'''
+<Usage>
+python prototype_transcribe.py input_location[1] output_location[2] project_name[3] batch_number(id)[4]
+'''
 if __name__ == '__main__':
 
-	transcribe_file(sys.argv[1], sys.argv[2], sys.argv[3])
+	input_file = sys.argv[1] + '/' + sys.argv[3] + '/' + sys.argv[4] + '.wav'
+	output_file = sys.argv[2] + '/' + sys.argv[3] + '/pred/' +  sys.argv[4] + '_pred.txt'
+	keyword_file = sys.argv[2] + '/' + sys.argv[3] + '/' +'keywords.txt'
+
+	transcribe_file(input_file, output_file, keyword_file)
